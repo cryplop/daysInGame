b7fa310be8be1d1f6fa6a59d022ed51d52449f81
cryplop <i.g.matsala@gmail.com>
2023-06-25 23:59:38 +0500

    Экран персонажа, содержимого нет... только экран

---
+++ app/src/main/java/com/example/daysingame/Character.kt
@@ -0,0 +1,12 @@
+package com.example.daysingame.com.example.daysingame
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.daysingame.R
+
+class Character : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.character_view)
+    }
+}
--- app/src/main/java/com/example/daysingame/MainActivity.kt
+++ app/src/main/java/com/example/daysingame/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.daysingame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import com.example.daysingame.com.example.daysingame.Character
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +16,10 @@ class MainActivity : AppCompatActivity() {
     }
     private lateinit var days: TextView
     fun quarterHasPassed(view: View) {
+
+        val intent = Intent(this@MainActivity, Character::class.java)
+        startActivity(intent)
+
         //Увеличиваем счетчик
         val countString = days.text.toString()
         var count: Int = Integer.parseInt(countString)
