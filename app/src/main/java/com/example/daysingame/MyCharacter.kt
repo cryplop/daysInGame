33756545424bc7bc1694637e862669a02c395bc0
cryplop <i.g.matsala@gmail.com>
2023-06-26 23:41:31 +0500

    попытка создать класс данных о персонаже

---
+++ app/src/main/java/com/example/daysingame/MyCharacter.kt
@@ -0,0 +1,9 @@
+package com.example.daysingame
+
+data class myCharacter(val name: String){
+    var nameCharacter:String = "Тора"
+    var talents = mapOf(
+        "wayBeast" to 2,
+        "masterManeuvering" to 1
+    )
+}
